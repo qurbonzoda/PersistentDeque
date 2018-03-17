d5246896a5792afad38f013ca2d89a75d1461049
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-17 19:51:39 +0300

    merge `smallDequeOptimization.arrayBuffer.bufferSize19SB` impl

---
+++ src/main/kotlin/immutableDeque/smallDequeOptimization/arrayBuffer/bufferSize19SB/level/SubStackBottomLevel.kt
@@ -0,0 +1,92 @@
+package immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize19SB.level
+
+import immutableDeque.ImmutableDeque
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize19SB.buffer.ImmutableBuffer
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize19SB.constants.RED
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize19SB.persistentDeque.DequeSubStack
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize19SB.persistentDeque.PersistentDeque
+
+internal open class SubStackBottomLevel(lhs: ImmutableBuffer,
+                                        rhs: ImmutableBuffer): ImmutableLevel(lhs, rhs, null) {
+    // MARK: ImmutableLevel
+    override fun withNewLhs(newLhs: ImmutableBuffer): SubStackBottomLevel {
+        return SubStackBottomLevel(newLhs, this.rhs)
+    }
+
+    override fun withNewRhs(newRhs: ImmutableBuffer): SubStackBottomLevel {
+        return SubStackBottomLevel(this.lhs, newRhs)
+    }
+
+    override fun subStackSize(depth: Int): Int {
+        return (this.lhs.size + this.rhs.size) shl depth
+    }
+
+    override fun subStackHeight(): Int {
+        return 1
+    }
+
+    override fun addBufferLeafValuesTo(list: MutableList<Any?>, depth: Int) {
+        this.lhs.addLeafValuesTo(list, depth)
+        this.rhs.addLeafValuesTo(list, depth)
+    }
+
+    override fun getBufferLeafValueAt(index: Int, size: Int, depth: Int): Any? {
+//        assert(index < size)
+
+        val lhsSize = this.lhs.size shl depth
+        if (index < lhsSize) {
+            return this.lhs.getLeafValueAt(index, depth)
+        }
+        return this.rhs.getLeafValueAt(index - lhsSize, depth)
+    }
+
+    override fun setBufferLeafValueAt(index: Int, value: Any?, size: Int, depth: Int): SubStackBottomLevel {
+//        assert(index < size)
+
+        val lhsSize = this.lhs.size shl depth
+        if (index < lhsSize) {
+            val newLhs = this.lhs.setLeafValueAt(index, value, depth)
+            return this.withNewLhs(newLhs)
+        }
+
+        val rhsSize = this.rhs.size shl depth
+        val newRhs = this.rhs.setLeafValueAt(rhsSize - (size - index), value, depth)
+        return this.withNewRhs(newRhs)
+    }
+
+    override fun <T> makeImmutableDeque(upperLhs: ImmutableBuffer,
+                                        upperRhs: ImmutableBuffer,
+                                        thisLhs: ImmutableBuffer,
+                                        thisRhs: ImmutableBuffer,
+                                        pDeque: PersistentDeque<*>,
+                                        newSize: Int): ImmutableDeque<T> {
+//        assert(upperLhs.color == GREEN && upperRhs.color == GREEN)
+//        assert(pDeque.next.stack.next != null && pDeque.next.next != null)
+
+        val lowerSubStack = pDeque.next.next
+
+        val newThis = SubStackBottomLevel(thisLhs, thisRhs)
+        val newNext = DequeSubStack(newThis, lowerSubStack)
+        return PersistentDeque(upperLhs, upperRhs, newNext, newSize)
+    }
+
+    override fun makeDequeSubStack(upperLhs: ImmutableBuffer,
+                                   upperRhs: ImmutableBuffer,
+                                   thisLhs: ImmutableBuffer,
+                                   thisRhs: ImmutableBuffer,
+                                   lowerSubStack: DequeSubStack?): DequeSubStack {
+//        assert(upperLhs.color == GREEN && upperRhs.color == GREEN)
+//        assert(lowerSubStack != null)
+
+        val newThis = SubStackBottomLevel(thisLhs, thisRhs)
+
+        return if (newThis.color == RED) {
+            val newUpper = SubStackBottomLevel(upperLhs, upperRhs)
+            DequeSubStack(newUpper, DequeSubStack(newThis, lowerSubStack))
+        } else {
+            val newUpper = NonBottomLevel(upperLhs, upperRhs, newThis)
+            DequeSubStack(newUpper, lowerSubStack)
+        }
+    }
+}
+
