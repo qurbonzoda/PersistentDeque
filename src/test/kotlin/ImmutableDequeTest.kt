6361f9fc8c5b014d9916c82d8fc1925b6b6b7380
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-17 19:16:14 +0300

    merge `smallDequeOptimization.arrayBuffer.bufferSize7S` impl

--- src/test/kotlin/ImmutableDequeTest.kt
+++ src/test/kotlin/ImmutableDequeTest.kt
@@ -1,5 +1,5 @@
 import immutableDeque.ImmutableDeque
-import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize7.emptyDeque.emptyDeque
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize7S.emptyDeque.emptyDeque
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertThrows
