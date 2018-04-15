17644235298e3c12e777f567374cfa0520d0f367
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-04-15 20:56:10 +0300

    add arrayBuffer.bufferSize61 arrayBuffer.bufferSize61S arrayBuffer.bufferSize64 arrayBuffer.bufferSize64S

---
+++ src/main/kotlin/immutableDeque/smallDequeOptimization/arrayBuffer/bufferSize64/buffer/LhsBuffer.kt
@@ -0,0 +1,134 @@
+package immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize64.buffer
+
+import immutableDeque.ImmutableDeque
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize64.constants.*
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize64.level.DequeBottomLevel
+
+internal class LhsBuffer(array: Array<Any?>,
+                         size: Int): ImmutableBuffer(array, size) {
+    // MARK: ImmutableBuffer
+    override fun withArray(array: Array<Any?>, size: Int): ImmutableBuffer {
+        if (size == 0) {
+            return LhsEmptyBuffer
+        }
+        return LhsBuffer(array, size)
+    }
+
+    override fun oppositeSideBufferWithArray(array: Array<Any?>, size: Int): ImmutableBuffer {
+        if (size == 0) {
+            return RhsEmptyBuffer
+        }
+        return RhsBuffer(array, size)
+    }
+
+    override fun addLeafValuesTo(list: MutableList<Any?>, depth: Int) {
+        for (i in this.size - 1 downTo 0) {
+            this.addLeavesOfNode(this.array[i], list, depth)
+        }
+    }
+
+    override fun getLeafValueAt(index: Int, depth: Int): Any? {
+//        assert(index < this.size shl depth)
+
+        val nodePosition = index shr depth
+        val indexInNode = index - (nodePosition shl depth)
+        val indexInArray = this.size - 1 - nodePosition
+        return this.getLeafOfNodeAt(indexInNode, this.array[indexInArray], depth)
+    }
+
+    override fun setLeafValueAt(index: Int, value: Any?, depth: Int): ImmutableBuffer {
+//        assert(index < this.size shl depth)
+
+        val nodePosition = index shr depth
+        val indexInNode = index - (nodePosition shl depth)
+        val indexInArray = this.size - 1 - nodePosition
+        val newNode = this.setLeafOfNodeAt(indexInNode, value, this.array[indexInArray], depth)
+
+        val newArray = arrayOfNulls<Any?>(MAX_BUFFER_SIZE)
+        System.arraycopy(this.array, 0, newArray, 0, this.size)
+        newArray[indexInArray] = newNode
+
+        return LhsBuffer(newArray, this.size)
+    }
+
+    override fun popAndPushAllToNextLevelBuffer(count: Int, nextLevelBuffer: ImmutableBuffer): ImmutableBuffer {
+//        assert(this.size % 2 == 0)
+
+        val newArray = arrayOfNulls<Any?>(MAX_BUFFER_SIZE)
+        System.arraycopy(nextLevelBuffer.array, 0, newArray, 0, nextLevelBuffer.size)
+
+        var position = nextLevelBuffer.size
+
+        for (i in 0 until this.size - count step 2) {
+            newArray[position++] = Pair(this.array[i + 1], this.array[i])
+        }
+        return LhsBuffer(newArray, nextLevelBuffer.size + ((this.size - count) shr 1))
+    }
+
+    override fun moveToUpperLevelBuffer(count: Int): ImmutableBuffer {
+        val newArray = arrayOfNulls<Any?>(MAX_BUFFER_SIZE)
+
+        var position = 0
+        val fromIndex = this.size - count
+
+        for (i in fromIndex until this.size) {
+            val pair = this.array[i] as Pair<*, *>
+            newArray[position++] = pair.second
+            newArray[position++] = pair.first
+        }
+        return LhsBuffer(newArray, count shl 1)
+    }
+
+    // MARK: ImmutableDeque
+
+    override val first: Any?
+        get() {
+            return this.array[this.size - 1]
+        }
+
+    override val last: Any?
+        get() {
+            return this.array[0]
+        }
+
+    override fun addFirst(value: Any?): ImmutableDeque<Any?> {
+        if (this.size + 1 < MAX_BUFFER_SIZE) {
+            return this.push(value)
+        }
+
+//        println(this.size + 1 - lastRegularizationSize)
+//        lastRegularizationSize = this.size + 1
+
+        val toMoveToRhs = FULL_BUFFER_DEQUE_SHOULD_MOVE_TO_OPPOSITE_SIDE
+        val toLeaveForLhs = this.size - toMoveToRhs
+
+        val rhsArray = arrayOfNulls<Any?>(MAX_BUFFER_SIZE)
+        var position = 0
+        for (i in toMoveToRhs - 1 downTo 0) {
+            rhsArray[position++] = this.array[i]
+        }
+        val rhs = RhsBuffer(rhsArray, toMoveToRhs)
+
+        val lhsArray = arrayOfNulls<Any?>(MAX_BUFFER_SIZE)
+        System.arraycopy(this.array, toMoveToRhs, lhsArray, 0, toLeaveForLhs)
+        lhsArray[toLeaveForLhs] = value
+        val lhs = LhsBuffer(lhsArray, toLeaveForLhs + 1)
+
+        return DequeBottomLevel(lhs, rhs)
+    }
+
+    override fun removeFirst(): ImmutableDeque<Any?> {
+        return this.pop()
+    }
+
+    override fun addLast(value: Any?): ImmutableDeque<Any?> {
+        val newArray = arrayOfNulls<Any?>(MAX_BUFFER_SIZE)
+        newArray[0] = value
+        val rhs = RhsBuffer(newArray, 1)
+        return DequeBottomLevel(this, rhs)
+    }
+
+    override fun removeLast(): ImmutableDeque<Any?> {
+        return this.removeBottomAndMoveRestToOppositeSideBuffer()
+    }
+}
---
+++ src/main/kotlin/immutableDeque/smallDequeOptimization/arrayBuffer/bufferSize64/buffer/RhsEmptyBuffer.kt
@@ -0,0 +1,82 @@
+package immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize64.buffer
+
+import immutableDeque.ImmutableDeque
+import immutableDeque.smallDequeOptimization.arrayBuffer.bufferSize64.constants.MAX_BUFFER_SIZE
+
+internal object RhsEmptyBuffer: ImmutableBuffer(arrayOfNulls(MAX_BUFFER_SIZE), 0) {
+    // MARK: ImmutableBuffer
+    override fun withArray(array: Array<Any?>, size: Int): ImmutableBuffer {
+        if (size == 0) {
+            return this
+        }
+        return RhsBuffer(array, size)
+    }
+
+    override fun oppositeSideBufferWithArray(array: Array<Any?>, size: Int): ImmutableBuffer {
+        if (size == 0) {
+            return LhsEmptyBuffer
+        }
+        return LhsBuffer(array, size)
+    }
+
+    override fun addLeafValuesTo(list: MutableList<Any?>, depth: Int) {
+        // do nothing
+    }
+
+    override fun getLeafValueAt(index: Int, depth: Int): Any? {
+        throw IndexOutOfBoundsException()
+    }
+
+    override fun setLeafValueAt(index: Int, value: Any?, depth: Int): ImmutableBuffer {
+        throw IndexOutOfBoundsException()
+    }
+
+    override fun popAndPushAllToNextLevelBuffer(count: Int, nextLevelBuffer: ImmutableBuffer): ImmutableBuffer {
+//        assert(count == 0)
+        return nextLevelBuffer
+    }
+
+    override fun moveToUpperLevelBuffer(count: Int): ImmutableBuffer {
+//        assert(count == 0)
+        return this
+    }
+
+    override fun prependSavingOrder(buffer: ImmutableBuffer): ImmutableBuffer {
+        return buffer
+    }
+
+    // MARK: ImmutableDeque
+    override fun isEmpty(): Boolean = true
+    override val first: Any? = null
+    override val last: Any? = null
+
+    override fun addFirst(value: Any?): ImmutableDeque<Any?> {
+        val newArray = arrayOfNulls<Any?>(MAX_BUFFER_SIZE)
+        newArray[0] = value
+        return LhsBuffer(newArray, 1)
+    }
+
+    override fun removeFirst(): ImmutableDeque<Any?> {
+        throw NoSuchElementException()
+    }
+
+    override fun addLast(value: Any?): ImmutableDeque<Any?> {
+        return push(value)
+    }
+
+    override fun removeLast(): ImmutableDeque<Any?> {
+        throw NoSuchElementException()
+    }
+
+    override fun toList(): List<Any?> {
+        return emptyList()
+    }
+
+    override fun get(index: Int): Any? {
+        throw IndexOutOfBoundsException()
+    }
+
+    override fun set(index: Int, value: Any?): ImmutableDeque<Any?> {
+        throw IndexOutOfBoundsException()
+    }
+}
